60ec1a4a9285404be496863b426d7fcb1aa4efa9
Chintya-aa <tyachintyachin@gmail.com>
2025-05-17 14:47:28 +0700

    memasukkan class universitas

--- friendClass.cpp
+++ friendClass.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class Mahasiswa;
 
-class Mahasiswa 
+class Mahasiswa
 {
 private:
     string nama;
@@ -14,17 +14,15 @@ private:
 
 public:
     Mahasiswa(string n, string nim) : nama(n), NIM(nim), nilai(0) {}
-
-    // Method untuk menampilkan nilai
-    void tampilkanNilai() {
+    void tampilkanNilai()
+    {
         cout << "Nilai " << nama << ": " << nilai << endl;
     }
 
-    // Access value using friend function
-    friend void beriNilai(Mahasiswa& m, float nilai);
+    friend void beriNilai(Mahasiswa &m, float nilai);
 };
 
-class Dosen 
+class Dosen
 {
 private:
     string nama;
@@ -34,6 +32,25 @@ private:
 
 public:
     Dosen(string n, string id, string p, float g) : nama(n), NIDN(id), pangkat(p), gaji(g) {}
-    friend void beriNilai(Mahasiswa& m, float nilai);
-    friend void ubahPangkat(Dosen* d, string pangkatBaru);
+    friend void beriNilai(Mahasiswa &m, float nilai);
+    friend void ubahPangkat(Dosen *d, string pangkatBaru);
 };
+
+class Universitas
+{
+public:
+    void lihatGajiDosen(Dosen *d)
+    {
+        cout << "Gaji Dosen " << d->nama << ": " << d->gaji << endl;
+    }
+};
+
+void beriNilai(Mahasiswa &m, float n)
+{
+    m.nilai = n;
+}
+
+void ubahPangkat(Dosen *d, string pangkatBaru)
+{
+    d->pangkat = pangkatBaru;
+}
