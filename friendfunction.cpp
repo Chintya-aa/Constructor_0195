126921f125ea6a503ae9dcb830b80cf5bd1f53dd
Chintya-aa <tyachintyachin@gmail.com>
2025-05-17 15:17:34 +0700

    memasukkan class mahasiswa

--- friendfunction.cpp
+++ friendfunction.cpp
@@ -6,5 +6,24 @@ class Dosen;
 class Staff;
 class Universitas;
 
+class Mahasiswa {
+private:
+    string nama;
+    string NIM;
+    float nilai;
+    
+public:
+    Mahasiswa(string nama, string NIM) {
+        this->nama = nama;
+        this->NIM = NIM;
+        this->nilai = 0.0;
+    }
+    
+    void tampilInfo() {
+        cout << "Mahasiswa: " << nama << " (NIM: " << NIM << "), Nilai: " << nilai << endl;
+    }
+    
+    friend class Dosen;
+};
 
 
